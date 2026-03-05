2724be04ee7919591019c37f0f0ed9267a581693
Yehuda Ringler <yrappdev@gmail.com>
2026-03-04 21:39:02 -0500

    Fix index xml files

--- src/_data/xmlFiles.js
+++ src/_data/xmlFiles.js
@@ -2,7 +2,7 @@ const { readdirSync } = require("fs");
 const { resolve } = require("path");
 
 module.exports = function () {
-  const root = resolve(__dirname, "../..");
+  const root = resolve(__dirname, "../texts");
   return readdirSync(root)
     .filter((f) => /^chapter_.*\.xml$/.test(f))
     .sort();
